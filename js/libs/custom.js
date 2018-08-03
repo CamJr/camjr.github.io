74776eb0508e117b25862f71477366e61d2f636e
CamJr <cfontainef1@gmail.com>
2018-08-03 13:45:22 +0200

    0.0.1

---
+++ js/libs/custom.js
@@ -0,0 +1,20 @@
+var userFeed = new Instafeed({
+  get: 'user',
+  userId: '1697317359',
+  limit: 8,
+  resolution: 'low_resolution',
+  accessToken: '1697317359.1677ed0.035030b365cf49769bb3f7f3099469e8',
+  sortBy: 'most-recent',
+  template: '<div class="center col s3 instaimg"><a href="{{link}}" title="{{caption}}" target="_blank"><img src="{{image}}" alt="{{caption}}" class="img-fluid"/></a></div>',
+});
+
+  userFeed.run();
+
+  // This will create a single gallery from all elements that have class "gallery-item"
+  $('.gallery').magnificPopup({
+    type: 'image',
+    delegate: 'a',
+    gallery: {
+      enabled: true
+    }
+  });
